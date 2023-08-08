5acd255feaa98eb04fd69a64a5d4944d8e21fafe
Donizete Junior <donizete.junior>
2023-08-08 16:57:12 -0300

    feat: Update prettierr

--- bellbuy/src/main.tsx
+++ bellbuy/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/* eslint-disable simple-import-sort/imports */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
