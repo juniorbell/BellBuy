dd7e534e0d9dd5833b58c05bf4f1a135c157aa3a
Donizete Junior <donizete.junior>
2023-08-22 16:59:15 -0300

    Feat: Footer

--- bellbuy/src/main.tsx
+++ bellbuy/src/main.tsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './main.css';
+import Footer from './modules/footer/footer';
 import { GlobalProvider } from './shared/hooks/useGlobalContext';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalProvider>
       <App />
+      <Footer />
     </GlobalProvider>
   </React.StrictMode>,
 );
---
+++ bellbuy/src/modules/footer/footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-logo">
+                <img src="/logo-bellsystem.png" alt="Logo" />
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
